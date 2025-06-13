5c159ffecda64a23b3a95c73b79790fcf2a9ead1
Asif Khan <iamasifkhanbk@gmail.com>
2025-06-13 17:29:01 +0530

    Typo fix

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2025 Asif Khan B K asifkhaan20064@gmail.com
 */
 package main
 
-import "gihub/asifkhanbk/cost-estimator/cmd"
+import "github/asifkhanbk/cost-estimator/cmd"
 
 func main() {
 	cmd.Execute()
